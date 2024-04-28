2c988e2ebd25500343effc6325de3134f534a5b0
NinoPapava <gv.papava@gmail.com>
2024-04-28 12:48:30 +0400

    show Context text - right click

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,10 +1,28 @@
 import '../styles/Menu.scss'
+import { useState } from "react";
 
 const Menu = ({ title }) => {
+  const [isClicked, setIsClicked] = useState(false);
+  const [isContext, setIsContext] = useState({ x: 0, y: 0 });
+
+  const showContext = (e) => {
+    e.preventDefault();
+    setIsContext({ x: e.clientX, y: e.clientY });
+    setIsClicked(true);
+  };
+
+  const hideContext = () => {
+    setIsClicked(false);
+  };
 
   return (
     <>
-      {title}
+      <div onContextMenu={showContext}>{title}</div>
+      {isClicked && (
+        <div className="context-menu" style={{ top: isContext.y, left: isContext.x }} onClick={hideContext}>
+          Context Menu for {title}
+        </div>
+      )}
     </>
   )
 }
